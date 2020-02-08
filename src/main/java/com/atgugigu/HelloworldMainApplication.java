5dd4b276ea44504a86c8ceb3757c4b793beaf666
sora <1336395671@qq.com>
2020-02-08 17:03:30 +0800

    第一次提交

---
+++ src/main/java/com/atgugigu/HelloworldMainApplication.java
@@ -0,0 +1,12 @@
+package com.atgugigu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //标注主程序类
+public class HelloworldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldMainApplication.class,args);
+    }
+}
